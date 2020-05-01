559661ce6432a3dbe9521c0abfdd37e41ff547d6
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-05-01 09:41:44 +0530

    Login Done :: Admin Side

--- Engineering_Tools/ET_Admin/urls_admin.py
+++ Engineering_Tools/ET_Admin/urls_admin.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ET_login
+from .views import ET_login, ET_Home
 app_name = 'Admin'
 
 urlpatterns = [
     path('',ET_login, name="Admin_login"),
+    path('Home/', ET_Home, name="Admin_home"),
 ]
--- Engineering_Tools/ET_Admin/views.py
+++ Engineering_Tools/ET_Admin/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def ET_login(request):
     temp = "ET_Admin/login.html"
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username)
+        print('\n')
+        print(password)
+
+        if username == "Admin" and password == "Admin":
+            print("Admin Login Done")
+            return redirect("Admin:Admin_home")
+        else:
+            print("Username and password not match, Login Fail")
+
+    return render(request,temp)
+
+
+def ET_Home(request):
+    temp = "ET_Admin/index.html"
     return render(request,temp)
