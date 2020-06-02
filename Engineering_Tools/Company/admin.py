2041fa91fc64ec69d82a4c5272e880c4ad796f5f
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-06-02 19:52:41 +0530

    Font Awesome Icon Updated :: Company Side

--- Engineering_Tools/Company/admin.py
+++ Engineering_Tools/Company/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Register, Company_Profile, Product_Catagory, Product_Hardware
+from .models import Register, Company_Profile, Product_Catagory, Product_Hardware, Product_software
 
 # Register your models here.
 admin.site.register(Register)
 admin.site.register(Company_Profile)
 admin.site.register(Product_Catagory)
 admin.site.register(Product_Hardware)
+admin.site.register(Product_software)
