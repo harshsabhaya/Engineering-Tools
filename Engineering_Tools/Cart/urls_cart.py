781a7a62bcb68615505f9a913e6488d5aa55d879
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-06-19 13:18:35 +0530

    Remove From WishList From Product Details View Done :: User & Cart Side

--- Engineering_Tools/Cart/urls_cart.py
+++ Engineering_Tools/Cart/urls_cart.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import  Add_To_Wish_List_View
+from .views import  Add_To_Wish_List_View, Remove_From_Wish_List_View
 app_name = "Cart"
 
 urlpatterns = [
     path('Add_To_Wishlist', Add_To_Wish_List_View, name="Add_To_WishList"),
+    path('Remove_From_Wishlist', Remove_From_Wish_List_View, name="Remove_From_WishList"),
 
 ]
--- Engineering_Tools/Cart/views.py
+++ Engineering_Tools/Cart/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from Company.models import Product_Hardware
 from . models import Wishlist
 # Create your views here.
+
 def Add_To_Wish_List_View(request):
     if request.method == "POST":
         product_name = request.POST.get("productName")
@@ -12,6 +13,15 @@ def Add_To_Wish_List_View(request):
         product = Product_Hardware.objects.get(id = P_ID)
         user = request.user
         Wishlist.objects.create(product_name=product_name, user_name=user_name, user_email=user_email, price=price, product_id=product.id, user_id=user.id)
-        return redirect("User:Wishlist")
+        return redirect(reverse('User:Product_Details', args=(P_ID,)))
+    else:
+        return redirect("User:Home")
+
+
+def Remove_From_Wish_List_View(request):
+    if request.method == "POST":
+        P_ID = request.POST.get("productId")
+        Wishlist.objects.get(product= P_ID, user=request.user).delete()
+        return redirect(reverse('User:Product_Details', args=(P_ID,)))
     else:
         return redirect("User:Home")
