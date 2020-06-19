8936cae434e9db6348dd3f0f8892a0f94898cdd1
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-06-19 10:48:14 +0530

    Wishlist Model Created :: Cart Side

--- Engineering_Tools/Cart/models.py
+++ Engineering_Tools/Cart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from Company.models import Product_Hardware
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Wishlist(models.Model):
+    product = models.ForeignKey(Product_Hardware, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    product_name = models.CharField(max_length = 20)
+    user_name = models.CharField(max_length = 20)
+    user_email = models.EmailField()
+    price = models.CharField(max_length=10)
+    added_in_wishlist = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name + " " + self.user_name
+
