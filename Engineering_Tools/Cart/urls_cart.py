6c5bda858b5be197e818d6ec2b81799f54c2dc3c
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-06-19 11:20:44 +0530

    Add To Wishlist Logic Done :: User & Cart Side

--- Engineering_Tools/Cart/urls_cart.py
+++ Engineering_Tools/Cart/urls_cart.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import  Add_To_Wish_List_View
 app_name = "Cart"
 
 urlpatterns = [
+    path('Add_To_Wishlist', Add_To_Wish_List_View, name="Add_To_WishList"),
+
 ]
--- Engineering_Tools/Cart/views.py
+++ Engineering_Tools/Cart/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from Company.models import Product_Hardware
+from . models import Wishlist
 # Create your views here.
+def Add_To_Wish_List_View(request):
+    if request.method == "POST":
+        product_name = request.POST.get("productName")
+        user_name = request.user.username
+        user_email = request.user.email
+        price = request.POST.get("productPrice")
+        P_ID = request.POST.get("productId")
+        product = Product_Hardware.objects.get(id = P_ID)
+        user = request.user
+        Wishlist.objects.create(product_name=product_name, user_name=user_name, user_email=user_email, price=price, product_id=product.id, user_id=user.id)
+        return redirect("User:Wishlist")
+    else:
+        return redirect("User:Home")
